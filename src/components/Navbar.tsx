7f6a4fa962bd4d802b7bad52484d2d4a36f8859b
sammy6378 <samuel202mwangi@gmail.com>
2025-02-27 15:59:49 +0300

    Implement Navbar with login and registration modals, and theme toggle

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,43 @@
-import Link from 'next/link'
-import React from 'react'
+'use client'
+import { useState } from 'react';
+import Link from 'next/link';
+import Modal from './lib/modalProps'; 
+import Login from '@/app/auth-user/login/page';
+import Register from '@/app/auth-user/signup/page';
+import ThemeToggle from '@/app/themes/toggleButton';
+import Button  from '@mui/material/Button';
 
 function Navbar() {
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
+  const [isRegisterOpen, setIsRegisterOpen] = useState(false);
+
+  const handleLoginOpen = () => setIsLoginOpen(true);
+  const handleLoginClose = () => setIsLoginOpen(false);
+
+  const handleRegisterOpen = () => setIsRegisterOpen(true);
+  const handleRegisterClose = () => setIsRegisterOpen(false);
+
   return (
-    <nav className='flex justify-between items-center h-16 bg-white text-black relative shadow-sm font-mono' role='navigation'>
+    <nav className="flex justify-between items-center h-16 border-b border-gray-500 sticky top-0 z-50" role="navigation">
       <div>
-          <Link href={'/'} className='pl-8'>Logo</Link>
+        <Link href="/" className="pl-8">Logo</Link>
       </div>
-        <ul className='flex items-center space-x-4 px-4'>
-            <Link href={'/'}>Home</Link>
-            <Link href={'/about'}>About</Link>
-            <Link href={"/contact"}>Contact</Link>
-            <Link href={"/blogs"}>Blog</Link>
-        </ul>
+      <div className="flex justify-between items-center space-x-4 pr-8">
+        <Button variant="text"  onClick={handleLoginOpen}>Login</Button>
+        <Button variant="outlined" onClick={handleRegisterOpen}>Register</Button>
+        <ThemeToggle />
+      </div>
+
+      {/* Modals */}
+      <Modal isOpen={isLoginOpen} onClose={handleLoginClose}>
+        <Login />
+      </Modal>
+
+      <Modal isOpen={isRegisterOpen} onClose={handleRegisterClose}>
+        <Register />
+      </Modal>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
