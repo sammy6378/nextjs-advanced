b70bf7e44988d3815c824aad8eca19c2d8977b6f
sammy6378 <samuel202mwangi@gmail.com>
2025-02-27 15:59:19 +0300

    Add Sign Up component with form validation and submission handling

---
+++ src/app/auth-user/signup/page.tsx
@@ -0,0 +1,78 @@
+'use client'
+
+import { useFormik } from 'formik';
+import { formSchema } from '@/components/schemas/formSchema';
+import '../../globals.css';
+
+interface FormValues {
+  username: string;
+  email: string;
+  password: string;
+}
+
+const onSubmit = (values: FormValues) => {
+  console.log('Form Submitted', values);
+};
+
+function Register() {
+  const { values, handleBlur, errors, touched, handleChange, handleSubmit } = useFormik({
+    initialValues: { username: '', email: '', password: ''},
+    validationSchema: formSchema,
+    onSubmit,
+  });
+
+  return (
+    <>
+      <h2 className="text-3xl font-bold text-purple-900 mb-4">Sign Up</h2>
+      <form onSubmit={handleSubmit} className="space-y-2">
+        {/* Username Field */}
+        <div>
+          <label htmlFor="username" className="input-label">Username</label>
+          <input
+            type="text"
+            value={values.username}
+            onChange={handleChange}
+            id="username"
+            onBlur={handleBlur}
+            placeholder="Enter your username"
+            className={`input-container ${errors.username && touched.username ? 'input-error' : ''}`}
+          />
+          {errors.username && touched.username && <div className="error-message">{errors.username}</div>}
+        </div>
+
+        {/* Email Field */}
+        <div>
+          <label htmlFor="email" className="input-label">Email</label>
+          <input
+            type="email"
+            value={values.email}
+            onChange={handleChange}
+            id="email"
+            onBlur={handleBlur}
+            placeholder="Enter your email"
+            className={`input-container ${errors.email && touched.email ? 'input-error' : ''}`}
+          />
+          {errors.email && touched.email && <div className="error-message">{errors.email}</div>}
+        </div>
+
+        {/* Password Field */}
+        <div>
+          <label htmlFor="password" className="input-label">Password</label>
+          <input
+            type="password"
+            value={values.password}
+            onChange={handleChange}
+            id="password"
+            onBlur={handleBlur}
+            placeholder="Enter your password"
+            className={`input-container ${errors.password && touched.password ? 'input-error' : ''}`}
+          />
+          {errors.password && touched.password && <div className="error-message">{errors.password}</div>}
+        </div>
+        <button type="submit" className="submit-button">Sign Up</button>
+      </form>
+    </>
+  );
+}
+
+export default Register;
